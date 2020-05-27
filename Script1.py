c0381798904efaae52b8a1f0d1d381ab93d27ade
Kamil Czernuszewicz <kamil@czernuszewicz.pl>
2020-05-27 23:10:59 +0200

    Task #1

--- Script1.py
+++ Script1.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 df_results = pd.read_csv('survey_results_public.csv',
-                         usecols=['Age1stCode', 'YearsCode', 'YearsCodePro', 'ConvertedComp', 'Age'])
+                         usecols=['Respondent', 'Age1stCode', 'YearsCode', 'YearsCodePro', 'ConvertedComp', 'Age'],
+                         index_col='Respondent')
 df_results.dropna(inplace=True)
 
 print(pd.unique(df_results[['Age1stCode']].values.ravel()))
@@ -17,5 +18,6 @@ df_results.replace(to_replace={'Younger than 5 years': '4',
 df_results.replace(to_replace={'Less than 1 year': '0',
                               'More than 50 years': '51'},
                   inplace=True)
+
 
 
